619d5fe4da4fa76889e2c603bab3e40981c0c0ae
khdscor <khds_cor@naver.com>
2022-09-04 22:19:59 +0900

    [ADD] 마이페이지 위시리스트 조회 구현

--- frontend/src/component/mypage/MyPage.js
+++ frontend/src/component/mypage/MyPage.js
@@ -7,11 +7,14 @@ import CancelButton from "./../CancelButton";
 import InfBox from "./InfBox";
 import SubFuncBox from "./SubFuncBox";
 import findUserDetailsApi from "./../../api/user/findUserDetailsApi";
+import WishBoards from "./wishboards/WishBoards";
 
 const Outside = styled.div`
   width: 1050px;
+  height: 400px;
   margin: 10px auto;
   border: 5px solid blue;
+  overflow: auto;
 `;
 const MyInf = styled.div`
   width: 1050px;
@@ -30,7 +33,17 @@ function Mypage() {
   }
   const navigate = useNavigate();
   const [userInf, setUserInf] = useState();
-
+  const [isBoardsOpened, setIsBoardsOpened] = useState(false);
+  const [wishBoards, setWishBoards] = useState([
+    { id: 1, name: "ㅎㅇㅎㅇ" },
+    { id: 2, name: "ㅎㅇㅎㅇ" },
+    { id: 3, name: "ㅎㅇㅎㅇ" },
+    { id: 4, name: "ㅎㅇㅎㅇ" },
+    { id: 5, name: "ㅎㅇㅎㅇ" },
+    { id: 6, name: "ㅎㅇㅎㅇ" },
+    { id: 7, name: "ㅎㅇㅎㅇ" },
+    { id: 8, name: "ㅎㅇㅎㅇ" },
+  ]);
   useEffect(() => {
     findUserDetailsApi(sessionStorage.getItem(ACCESS_TOKEN)).then((resp) => {
       setUserInf(resp);
@@ -38,25 +51,34 @@ function Mypage() {
   }, []);
 
   return (
-    <Outside>
-      <MyInf>
-        <CancelButton navigate={navigate} />
-        <BaseImg>
-          <img
-            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-            alt="my image"
-            style={{
-              marginLeft: "20px",
-              width: "300px",
-              height: "300px",
-              borderRadius: "1000px",
-            }}
-          />
-        </BaseImg>
-        <InfBox userInf={userInf} />
-        <SubFuncBox navigate={navigate} userInf={userInf} />
-      </MyInf>
-    </Outside>
+    <>
+      <Outside>
+        <MyInf>
+          <CancelButton navigate={navigate} />
+          <BaseImg>
+            <img
+              src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              alt="my image"
+              style={{
+                marginLeft: "20px",
+                width: "300px",
+                height: "300px",
+                borderRadius: "1000px",
+              }}
+            />
+          </BaseImg>
+          <InfBox userInf={userInf} />
+          <SubFuncBox navigate={navigate} userInf={userInf} />
+        </MyInf>
+      </Outside>
+      <Outside>
+        <WishBoards
+          wishBoards={wishBoards}
+          isOpened={isBoardsOpened}
+          setIsOpened={setIsBoardsOpened}
+        />
+      </Outside>
+    </>
   );
 }
 
---
+++ frontend/src/component/mypage/wishboards/WishContent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const WishContentStyle = styled.div`
+  display: flex;
+  justify: center;
+  height: 50px;
+  margin-bottom: 6px;
+  margin-left: 10px;
+  font-size: 40px;
+  border-bottom: 2px solid rgb(90, 155, 213);
+  cursor: pointer;
+`;
+
+const WishContent = (props) => {
+  return (
+    <WishContentStyle
+      onClick={() => {
+        props.navigate();
+      }}
+    >
+      {props.wish.name}
+    </WishContentStyle>
+  );
+};
+
+export default WishContent;
